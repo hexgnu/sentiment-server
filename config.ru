1b89295d5e9296f644839f51ba40cc19242253d3
Matt Kirk <matt@matthewkirk.com>
2011-12-21 13:25:40 -0800

    Fixing up this ish so that it works with ports...

--- config.ru
+++ config.ru
@@ -1,9 +1,16 @@
+require 'yaml'
 require "rubygems"
 require "bundler"
-require 'yaml'
 Bundler.require
 require './guise/lib/guise'
 require './lib/db'
 require './lib/model'
 
 require './guise-service'
+
+run Guise::Service
+
+
+EM.defer do
+  SentimentEngine = Guise::Sentiment.new
+end
--- guise-service.rb
+++ guise-service.rb
@@ -1,77 +1,75 @@
 require 'dalli'
-EM.run do
-  module Guise
-    class Service < Sinatra::Base
-      register Sinatra::Async
-      def authorized?
-        if request.env['rack.request.query_hash']['key'].to_s == 'd4c524b30affeb984f3ed535a993e5b5b05ce431'
-          true
-        else
-          false
-        end
+module Guise
+  class Service < Sinatra::Base
+    register Sinatra::Async
+    def authorized?
+      if request.env['rack.request.query_hash']['key'].to_s == 'd4c524b30affeb984f3ed535a993e5b5b05ce431'
+        true
+      else
+        false
       end
-      
-      def json_correct(request, keys)
-        json = request.env["rack.input"].read
-        
-        if json.nil?
-          error(400, {:message => "JSON cannot be blank"}.to_json) 
-        end
-        
-        begin
-          json = JSON.parse(json)
-        rescue Exception => e
-          error(400, {:message => "Json is malformed: #{e.message}"}.to_json)
-        end
-        
-        
-        keys_there = keys.map {|k| json.has_key?(k)}.uniq
-        
-        if keys_there.include?(false)
-          error(400, {:message => "JSON must have #{keys.join(' ')} assigned"}.to_json)
-        end
-        
-        json
-      end
-      
-      aget '/' do
-        body("Battleshields operational")
-      end
+    end
     
-      apost '/vote.json', :provides => :json do
-        error(401, {:message => "You are not authorized to post this"}.to_json) unless authorized?
+    def json_correct(request, keys)
+      json = request.env["rack.input"].read
       
-        json = json_correct(request, %w[sentiment text])
-        
-        begin
-          @response_code = SentimentEngine.vote(json['sentiment'], json['text'])
-        rescue => e
-          error(500, {:message => e.message}.to_json)
-        end
-        
-        body({:message => @response_code}.to_json)
+      if json.nil?
+        error(400, {:message => "JSON cannot be blank"}.to_json) 
       end
+      
+      begin
+        json = JSON.parse(json)
+      rescue Exception => e
+        error(400, {:message => "Json is malformed: #{e.message}"}.to_json)
+      end
+      
+      
+      keys_there = keys.map {|k| json.has_key?(k)}.uniq
+      
+      if keys_there.include?(false)
+        error(400, {:message => "JSON must have #{keys.join(' ')} assigned"}.to_json)
+      end
+      
+      json
+    end
     
-      apost '/predict.json' do
-        content_type 'application/json', :charset => 'utf-8'
-        json = json_correct(request, %w[text])
-        body({:sentiment => SentimentEngine.predict(json['text'])}.to_json)
+    aget '/' do
+      body("Battleshields operational")
+    end
+  
+    apost '/vote.json', :provides => :json do
+      error(401, {:message => "You are not authorized to post this"}.to_json) unless authorized?
+    
+      json = json_correct(request, %w[sentiment text])
+      
+      begin
+        @response_code = SentimentEngine.vote(json['sentiment'], json['text'])
+      rescue => e
+        error(500, {:message => e.message}.to_json)
       end
       
-      apost '/predictMany.json' do
-        content_type 'application/json', :charset => 'utf-8'
-        begin
-          json = json_correct(request, %w[texts])
-          puts json.inspect
-          @resp = SentimentEngine.predict_many(json['texts'])
-          body(@resp.to_json)
-        rescue => e
-          body(e.backtrace)
-        end
+      body({:message => @response_code}.to_json)
+    end
+  
+    apost '/predict.json' do
+      content_type 'application/json', :charset => 'utf-8'
+      json = json_correct(request, %w[text])
+      body({:sentiment => SentimentEngine.predict(json['text'])}.to_json)
+    end
+    
+    apost '/predictMany.json' do
+      content_type 'application/json', :charset => 'utf-8'
+      begin
+        json = json_correct(request, %w[texts])
+        puts json.inspect
+        @resp = SentimentEngine.predict_many(json['texts'])
+        body(@resp.to_json)
+      rescue => e
+        body(e.backtrace)
       end
     end
   end
-  Guise::Cache = Dalli::Client.new
-  SentimentEngine = Guise::Sentiment.new
-  Guise::Service.run!
 end
+
+Guise::Cache = Dalli::Client.new
+
